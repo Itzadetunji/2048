cd37823ebf2eca6d5dfdf72692804801015dbf4b
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-06-23 03:04:41 +0100

    Created Stack

--- 2048.py
+++ 2048.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import colors as c
+import random
 
 
 class Game(tk.Frame):
@@ -12,33 +13,89 @@ class Game(tk.Frame):
             self, bg=c.GRID_COLOR, bd=3, width=600, height=600
         )
         self.main_grid.grid(pady=(100, 0))
+        self.make_GUI()
+        self.start_game()
+
+        self.mainloop()
 
 
-def make_GUI(self):
-    # make grid
-    self.cells = []
-    for i in range (4)
-        row  = []
-        for j in range (4)
-            cell_frame = tk.Frame(
-                self.main_grid,
-                bg=c.EMPTY_CELL_COLOR,
-                width=150,
-                height=150
-            )       
-            cell_frame.grid(row=i, column=j, padx=5, pady=5)
-            cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
-            cell_number.grid(row=, column=j)
-            cell_data = {"frame": cell_frame, "number": cell_number}
-            row.append(cell_data)
-        self.cells.append(row)
+    def make_GUI(self):
+        # make grid
+        self.cells = []
+        for i in range (4):
+            row  = []
+            for j in range (4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg=c.EMPTY_CELL_COLOR,
+                    width=150,
+                    height=150
+                )       
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+        
+        # make score header
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=45, anchor="center")
+        tk.Label(
+            text="Score",
+            font=c.SCORE_LABEL_FONT
+        ).grid(row=0)
+        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
+        self.score_label.grid(row=1)
     
-    # make score header
-    score_frame = tk.Frame(self)
-    score_frame.place(relx=0.5, y=45, anchor="center")
-    tk.Label(
-        text="Score",
-        font=c.SCORE_LABEL_FONT
-    ).grid(row=0)
-    sef.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
-    self.score_label.grid(row=1)
+
+    def start_game(self):
+        # create matrix of zeros
+        self.matrix = [[0] * 4 for _ in range(4)]
+
+        #fill 2 random cells with 2s
+        row = random.randint(0,3)
+        col = random.randint(0,3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_COLORS[2],
+            text="2"
+        )
+        while(self.matrix[row][col] != 0):
+            row = random.randint(0,3)
+            col = random.randint(0,3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_COLORS[2],
+            text="2"
+        )
+
+        self.score = 0
+
+
+
+# Matrix Manipulation Functions
+
+def stack(self):
+    new_matrix = [[0] * 4 for _ in range(4)]
+    for i in range(4):
+        fill_position = 0
+        for j in range(4):
+            if self.matrix[i][j] != 0:
+                new_matrix[i][fill_position] = self.matrix[i][j]
+                fill_position += 1
+    self.matrix = new_matrix
+
+
+def combine(self):
+    for i in range(4):
+        for j in range(3):
+            if self.matrix[i][j] != 0 and self.matrix
+
+    
